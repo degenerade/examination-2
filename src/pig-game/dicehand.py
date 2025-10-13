8ec0c221a2cb6481597e3b063eed742d08079fa3
max-sellick <maxsellick@gmail.com>
2025-10-13 17:13:12 +0200

    UUM did some stuff

--- src/pig-game/dicehand.py
+++ src/pig-game/dicehand.py
@@ -1,6 +1,13 @@
 
+import dice
+
 
 class DiceHand:
 
     def __init__(self):
-        pass
+        self.previous_roll = 0
+
+    def get_roll(self, die: dice.Dice):
+        roll = die.roll_die()
+        self.previous_roll = roll
+        return roll
--- src/pig-game/game.py
+++ src/pig-game/game.py
@@ -1,5 +1,33 @@
 
+import player
+import intelligence
+import dicehand
+
+
 class Game:
 
     def __init__(self):
-        pass
+        self.players = []
+
+
+    def add_player(self, player_: player.Player):
+        self.players.append(player_)
+
+
+    def start(self, diehand: dicehand.DiceHand):
+        roll = 0
+        keep_going = True
+        
+        while keep_going:
+            roll = diehand.get_roll()
+            if roll == 1:
+                print("Dice rolled 0, you get no point this turn.")
+                break
+            
+            
+
+
+#roll dice
+# do 
+# roll dice
+# while != 1#
