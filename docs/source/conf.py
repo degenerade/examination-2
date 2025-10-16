73ccf5e8f1a3b1947c541f1bb9674a46cd9b12f5
max-sellick <maxsellick@gmail.com>
2025-10-16 14:20:54 +0200

    made into package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src")))
 
 project = 'pig-game'
 copyright = '2025, Group-42'
