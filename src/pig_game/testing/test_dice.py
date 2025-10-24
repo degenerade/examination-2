e9d26d00fedbad94b34b847db18b2e94a6b6119f
max-sellick <maxsellick@gmail.com>
2025-10-24 14:16:54 +0200

    made game testing class

--- src/pig_game/testing/test_dice.py
+++ src/pig_game/testing/test_dice.py
@@ -1,16 +1,16 @@
 """Unit testing for Dice class"""
 
 import unittest
-import dice
+from pig_game.dice import Dice
 
 
-class TestDiceClass(unittest.TestCase):
+class TestDice(unittest.TestCase):
     """Test the Dice class"""
 
     def testInitDefaultObject(self):
         """Instantiate a dice object and test its properties"""
-        die = dice.Dice()
-        self.assertIsInstance(die, dice.Dice)
+        die = Dice()
+        self.assertIsInstance(die, Dice)
 
         res = die.sides
         exp = 6
@@ -18,12 +18,12 @@ class TestDiceClass(unittest.TestCase):
 
     def test_roll_die(self):
         """Roll a dice and check its value is in bounds"""
-        die = dice.Dice()
+        die = Dice()
 
         res = die.roll_die()
         exp = 1 <= res <= die.sides
         self.assertTrue(exp)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
